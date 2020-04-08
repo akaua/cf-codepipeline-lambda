444c0bd1bdeff2eff1ed7a1ae281eba23508e6a0
akaua <akaua3269@gmail.com>
2020-04-08 00:35:41 -0300

    adjust codepipeline

--- delete_codepipeline_lambda_stack.sh
+++ delete_codepipeline_lambda_stack.sh
@@ -5,8 +5,8 @@ read -p 'Y/N: ' accept
 if [[ "$accept" == "Y" ]]; then
     echo "Deliting Codepipeline Lambda Stack CloudFormation."
     aws cloudformation delete-stack --stack-name CodePipelineLambdaStack
-    aws s3 rm s3://lambda-pipeline-artifact-bucket-archetype-akaua --recursive
-    aws s3api delete-bucket --bucket lambda-pipeline-artifact-bucket-archetype-akaua --region us-east-1
+    aws s3 rm s3://$BUCKET --recursive
+    aws s3api delete-bucket --bucket $BUCKET --region us-east-1
 else
     echo "Operation canceled."
 fi
